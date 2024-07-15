80f2ee58399cef2d28bfcaa2b0cfefc78c793998
alphasec <95212748+alphasecio@users.noreply.github.com>
2024-07-15 19:28:28 +0800

    Update index.py

--- index.py
+++ index.py
@@ -1,37 +1,58 @@
-import os, streamlit as st
+import os, tempfile, streamlit as st
+from llama_index.core import VectorStoreIndex
+from llama_parse import LlamaParse
 
-# Uncomment to specify your OpenAI API key here (local testing only, not in production!), or add corresponding environment variable (recommended)
-# os.environ['OPENAI_API_KEY']= ""
+# Streamlit app config
+st.subheader("Ask Llama")
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API key", type="password")
+    llama_cloud_api_key = st.text_input("LlamaCloud API key", type="password")
+    source_doc = st.file_uploader("Source document", type="pdf")
+col1, col2 = st.columns([4,1])
+query = col1.text_input("Query", label_visibility="collapsed")
 
-from llama_index import GPTSimpleVectorIndex, SimpleDirectoryReader, LLMPredictor, PromptHelper, ServiceContext
-from langchain.llms.openai import OpenAI
+# Session state initialization for documents and retrievers
+if  "loaded_doc" not in st.session_state or "query_engine" not in st.session_state:
+    st.session_state.loaded_doc = None
+    st.session_state.query_engine = None
 
-# Define a simple Streamlit app
-st.title("Ask Llama")
-query = st.text_input("What would you like to ask? (source: data/paul_graham_essay.txt)", "")
+submit = col2.button("Submit")
 
-# If the 'Submit' button is clicked
-if st.button("Submit"):
-    if not query.strip():
-        st.error(f"Please provide the search query.")
+# If the "Submit" button is clicked
+if submit:
+    if not openai_api_key.strip() or not llama_cloud_api_key.strip() or not query.strip():
+        st.error("Please provide the missing fields.")
+    elif not source_doc:
+        st.error("Please upload the source document.")
     else:
-        try:
-            # This example uses text-davinci-003 by default; feel free to change if desired
-            llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
-
-            # Configure prompt parameters and initialise helper
-            max_input_size = 4096
-            num_output = 256
-            max_chunk_overlap = 20
-
-            prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
-
-            # Load documents from the 'data' directory
-            documents = SimpleDirectoryReader('data').load_data()
-            service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-            index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+        with st.spinner("Please wait..."):
+            # Set API key environment variables
+            os.environ["OPENAI_API_KEY"] = openai_api_key
+            os.environ["LLAMA_CLOUD_API_KEY"] = llama_cloud_api_key
             
-            response = index.query(query)
-            st.success(response)
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
+            # Check if document has already been uploaded
+            if st.session_state.loaded_doc != source_doc:
+                try:
+                    # Initialize parser with markdown output (alternative: text)
+                    parser = LlamaParse(language="en", result_type="markdown")
+                    
+                    # Save uploaded file temporarily to disk, parse uploaded file, delete temp file
+                    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp_file:
+                        tmp_file.write(source_doc.read())
+
+                    documents = parser.load_data(tmp_file.name)
+                    os.remove(tmp_file.name)
+                    
+                    # Create a vector store index for uploaded file
+                    index = VectorStoreIndex.from_documents(documents)
+                    st.session_state.query_engine = index.as_query_engine()
+                    
+                    # Store the uploaded file in session state to prevent reloading
+                    st.session_state.loaded_doc = source_doc
+                except Exception as e:
+                    st.error(f"An error occurred: {e}")
+            try:
+                response = st.session_state.query_engine.query(query)
+                st.success(response)
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
